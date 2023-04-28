eeb0d124224fadb520d5a45646245790f5fda408
Attia Fouad <104443707+Attia-Fouad@users.noreply.github.com>
2023-04-28 18:16:12 +0300

    add some edits

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
@@ -23,16 +22,12 @@ class _LoginScreenState extends State<LoginScreen> {
         alignment: AlignmentDirectional.topStart,
         children: [
           Container(
-            height: double.infinity,
-            width: double.infinity,
             decoration: BoxDecoration(
               color: myColor,
             ),
           ),
           Container(
-            margin: const EdgeInsets.only(left: 30.0, bottom: 40.0),
-            height: double.infinity,
-            width: double.infinity,
+            margin: const EdgeInsets.only(left: 20.0, bottom: 30.0),
             decoration: const BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.only(
@@ -40,65 +35,78 @@ class _LoginScreenState extends State<LoginScreen> {
                 bottomRight: Radius.circular(60.0),
               ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 250, child: Image.asset('assets/logo.jpg')),
-                const SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: designedFormField(
-                    validator: (value) {},
-                    controller: mailController,
-                    type: TextInputType.emailAddress,
-                    label: 'Email',
-                    prefixIcon: Icons.email,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: designedFormField(
-                    validator: (value) {},
-                    controller: passwordController,
-                    type: TextInputType.emailAddress,
-                    label: 'Password',
-                    prefixIcon: Icons.email,
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Forget Password',
-                    style: TextStyle(
-                      color: HexColor('#d86a62'),
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 50,
                     ),
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Container(
-                  height: 55,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: myColor,
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  child: MaterialButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'LOGIN',
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+                    SizedBox(width: 250, child: Image.asset('assets/logo.jpg')),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: designedFormField(
+                        validator: (value) {},
+                        controller: mailController,
+                        type: TextInputType.emailAddress,
+                        label: 'Email',
+                        prefixIcon: Icons.email,
                       ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: designedFormField(
+                        validator: (value) {},
+                        controller: passwordController,
+                        type: TextInputType.visiblePassword,
+                        label: 'Password',
+                        prefixIcon: Icons.lock_outline,
+                        suffixIcon: Icons.remove_red_eye_outlined,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Forget Password',
+                        style: TextStyle(
+                          color: HexColor('#d86a62'),
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 55,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: myColor,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: MaterialButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'LOGIN',
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ],
